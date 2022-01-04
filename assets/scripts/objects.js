c6d2873d33ac3f8f456bc091ab99b9bc3e140ceb
Anna Harutyunyan <anuanharutyunyan>
2022-01-04 23:17:18 +0400

    Add: Search filter

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,7 +3,7 @@ const searchBtn = document.getElementById("search-btn");
 
 const movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = "") => {
   const movieList = document.getElementById("movie-list");
 
   if (movies.length === 0) {
@@ -12,7 +12,11 @@ const renderMovies = () => {
     movieList.classList.add("visible");
   }
   movieList.innerHTML = "";
-  movies.forEach((movie) => {
+
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter((movie) => movie.info.title.includes(filter));
+  filteredMovies.forEach((movie) => {
     const movieEl = document.createElement("li");
     let text = movie.info.title + " - ";
     for (const key in movie.info) {
@@ -48,4 +52,11 @@ const addMoviehandler = () => {
   renderMovies();
 };
 
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById("filter-title").value;
+
+  renderMovies(filterTerm);
+};
+
 addMovieBtn.addEventListener("click", addMoviehandler);
+searchBtn.addEventListener("click", searchMovieHandler);
